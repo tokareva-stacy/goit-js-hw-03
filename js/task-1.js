ea3a083464771e9569e7feb46aacf9ec706fe7a5
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-05-26 20:11:03 +0300

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,8 @@
+function slugify(title) {
+    return title.toLowerCase().split(" ").join("-");
+
+}
+
 console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
