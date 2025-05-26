a074ff8e251fa3b4b51089ac2edca03ca6885a40
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-05-26 19:50:37 +0300

    task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,14 @@
+function filterArray(numbers, value) {
+    const result = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            result.push(numbers[i]); 
+        }
+      }
+    
+      return result;
+}
+
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
